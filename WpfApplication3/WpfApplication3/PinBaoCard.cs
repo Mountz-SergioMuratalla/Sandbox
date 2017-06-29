4a5aad8d7ef3b41434739e476ba71ce481ac76a8
Manuel Sergio Muratalla Sanchez <sergio.muratalla@mountztorque.com>
2017-06-29 13:10:54 -0700

    3rd commit

--- WpfApplication3/WpfApplication3/PinBaoCard.cs
+++ WpfApplication3/WpfApplication3/PinBaoCard.cs
@@ -45,8 +45,9 @@ namespace WpfApplication3
         //[DllImport("inpout32.dll")]
         //private static extern char Inp32(short PortAddress);
         /// </summary>
-        
-        private j, k;
+
+        private byte i;
+        private int j, k;
 
 
 
@@ -64,64 +65,92 @@ namespace WpfApplication3
         /// End of declib + io function definitions
         /// /////////////////////////////////////////////////////////////
 
-            
-        public static int ad_addr = 4096;
-        public static int io_addr = 57344;
-        public static int m = GetDevicePortAddress(DECISION_PCI_8255_CARD, ref io_addr);
-        public static int ad13bit = 2048;   //'bin 0100000000000        this number starts at digit 12      MURA
-        public static int ad14bit = 4095;  // 'bin 0111111111111                                            MURA
+
+        private static int ad_addr = 4096;
+        private static int io_addr = 57344;
+        private static int m = GetDevicePortAddress(DECISION_PCI_8255_CARD, ref io_addr);
+        private static int ad13bit = 2048;   //'bin 0100000000000        this number starts at digit 12      MURA
+        private static int ad14bit = 4095;  // 'bin 0111111111111                                            MURA
 
         //'''io port addresses tag asignation
-        public static int P1A = io_addr + 0;
-        public static int P1B = io_addr + 1;
-        public static int P1C = io_addr + 2;
-        public static int P1CTRL = io_addr + 3;
+        private static int P1A = io_addr + 0;
+        private static int P1B = io_addr + 1;
+        private static int P1C = io_addr + 2;
+        private static int P1CTRL = io_addr + 3;
 
-        public static int P2A = io_addr + 4;
-        public static int P2B = io_addr + 5;
-        public static int P2C = io_addr + 6;
-        public static int P2CTRL = io_addr + 7;
+        private static int P2A = io_addr + 4;
+        private static int P2B = io_addr + 5;
+        private static int P2C = io_addr + 6;
+        private static int P2CTRL = io_addr + 7;
 
-        public static int CNT0 = io_addr + 8;
-        public static int CNT1 = io_addr + 9;
-        public static int CNT2 = io_addr + 10;
-        public static int CNT_CTRL = io_addr + 11;
+        private static int CNT0 = io_addr + 8;
+        private static int CNT1 = io_addr + 9;
+        private static int CNT2 = io_addr + 10;
+        private static int CNT_CTRL = io_addr + 11;
 
+        private static byte ZERO_SPEED = 0xF7;
+        private static byte STOP_SPEED = 0x8;
 
         //'''setting  up card 8255
+        //public static int k0 = DecOutb(P1CTRL, 0x8B);
+        //public static int k1 = DecOutb(P2CTRL, 0x9B);
+        //public static int k2 = DecOutb(P1A, 0xFF);
+        //public static int k3 = DecOutb(P1A, 0x0);
 
-        public static int k = DecOutb(P1CTRL, 0x8B);
-        public static int k1 = DecOutb(P2CTRL, 0x9B);
-        public static int k2 = DecOutb(P1A, 0xFF);
-        public static int k3 = DecOutb(P1A, 0x0);
-
-
+        public void Set8255() {
+            k = DecOutb(P1CTRL, 0x8B);
+            k = DecOutb(P2CTRL, 0x9B);
+            k = DecOutb(P1A, 0xFF);
+            k = DecOutb(P1A, 0x0);
+        }
 
         // set 8253
-        private static int k4 = DecOutb(CNT_CTRL, 0x30);
-        private static int k5 = DecOutb(CNT_CTRL, 0x70);
-        private static int k6 = DecOutb(CNT_CTRL, 0xB0);
-        private static byte i = DecInpb(CNT0), j = DecInpb(CNT0);
-        private static int k7 = DecOutb(CNT0, i), k8 = DecOutb(CNT0, j);
-        private static byte i1 = DecInpb(CNT1), j1 = DecInpb(CNT1);
-        private static int k9 = DecOutb(CNT1, i1), k10 = DecOutb(CNT1, j1);
+        //private static int k4 = DecOutb(CNT_CTRL, 0x30);
+        //private static int k5 = DecOutb(CNT_CTRL, 0x70);
+        //private static int k6 = DecOutb(CNT_CTRL, 0xB0);
+        //private static byte i = DecInpb(CNT0), j = DecInpb(CNT0);
+        //private static int k7 = DecOutb(CNT0, i), k8 = DecOutb(CNT0, j);
+        //private static byte i1 = DecInpb(CNT1), j1 = DecInpb(CNT1);
+        //private static int k9 = DecOutb(CNT1, i1), k10 = DecOutb(CNT1, j1);
 
-        //stop lamp
-        private static byte STOP_SPEED = 0x8;
-        private static byte ZERO_SPEED = 0xF7;
-        private static byte i3 = DecInpb(P1A);
-        private static int k11 = DecOutb(P1A, (byte)(i3 | STOP_SPEED));//'STOP_SPEED=&H8
+        public void Set8253(){
+            k = DecOutb(CNT_CTRL, 0x30);
+            k = DecOutb(CNT_CTRL, 0x70);
+            k = DecOutb(CNT_CTRL, 0xB0);
+            i = DecInpb(CNT0);
+            j = DecInpb(CNT0);
+            k = DecOutb(CNT0, i);
+            k = DecOutb(CNT0, j);
+            i = DecInpb(CNT1);
+            j = DecInpb(CNT1);
+            k = DecOutb(CNT1, i);
+            k = DecOutb(CNT1, j);
 
-        //stop lamp
-        public void StopLamp() {
-           i= DecInpb(P1A);
-           DecOutb(P1A, (byte)(i| STOP_SPEED));//'STOP_SPEED=&H8
         }
 
 
+        //stop lamp
 
-    
+        //private static byte i3 = DecInpb(P1A);
+        //private static int k11 = DecOutb(P1A, (byte)(i3 | STOP_SPEED));//'STOP_SPEED=&H8
 
+        //stop lamp
+           public void StopLamp() {
+            i= DecInpb(P1A);
+            DecOutb(P1A, (byte)(i| STOP_SPEED));//'STOP_SPEED=&H8
+        }
+
+
+        public int updateIO_addr()
+        {
+            m = GetDevicePortAddress(DECISION_PCI_8255_CARD, ref io_addr);
+            return m;
+        }
+
+        public int getIO_addr()
+        {
+            return m;
+        }
 
 
 
